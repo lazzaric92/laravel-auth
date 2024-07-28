c535174d5d41fad1f29df4b5c859257f9c13ac3a
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-28 17:51:00 +0200

    ordered projects by date in admin.projects.index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::orderBy('date', 'desc')->get();
         return view('admin.projects.index', compact('projects'));
     }
 
